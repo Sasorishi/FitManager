2a84d3206802af1817607cd3ccfcd6d0da412184
Sasorishi <on_alban@yahoo.fr>
2025-04-03 04:10:40 +0200

    fix: dashboard tables client

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -6,8 +6,6 @@ import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import ProtectedRoute from "@/services/auth/protectedRouteService";
 
-import data from "./data.json";
-
 export default function Page() {
   return (
     <ProtectedRoute>
@@ -25,7 +23,7 @@ export default function Page() {
           <div className="flex flex-1 flex-col">
             <div className="@container/main flex flex-1 flex-col gap-2">
               <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                <DataTable data={data} />
+                <DataTable />
               </div>
             </div>
           </div>
